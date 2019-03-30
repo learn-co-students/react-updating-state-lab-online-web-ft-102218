17b4643a2d6300a6edce985a6c70a732e099f6a2
github username <github email address>
2019-03-30 16:58:28 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import DigitalClicker from './components/DigitalClicker';
-import YouTubeDebugger from './components/YouTubeDebugger';
+import DigitalClicker from "./components/DigitalClicker";
+import YouTubeDebugger from "./components/YouTubeDebugger";
 
 ReactDOM.render(
   <div>
     <DigitalClicker />
     <YouTubeDebugger />
   </div>,
-  document.getElementById('global')
+  document.getElementById("global")
 );
+
+//    <YouTubeDebugger />
